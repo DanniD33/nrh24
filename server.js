997c94d1297821f82818ba63ff3db92db4a8e5b4
DanniD33 <danni.denmark7@gmail.com>
2024-06-14 00:23:32 -0400

    create first endpoint and set up input fields

--- server.js
+++ server.js
@@ -3,4 +3,21 @@ const { createServer } = require('node:http');
 
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3300;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(cors());
+
+
+//get main login
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'))
+})
+
+//Setting up server
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
